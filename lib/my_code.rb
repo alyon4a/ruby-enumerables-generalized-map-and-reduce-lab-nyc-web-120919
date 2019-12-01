4eed4a027e610be684b71202938525b33f5b31c1
Olena Matokhina <olena@matokhina.com>
2019-12-01 19:00:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ def reduce (source_array, starting_value = 0)
   
   i = 0
   while i < source_array.size do
-    result = yield(source_array[i], result)
+    result = yield(result, source_array[i])
     i += 1
   end
   return result

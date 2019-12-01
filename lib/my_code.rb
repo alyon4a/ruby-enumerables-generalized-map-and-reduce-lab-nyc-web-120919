9658e1f035d1f923375028bb52a6fe605032d4f9
Olena Matokhina <olena@matokhina.com>
2019-12-01 19:09:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,9 +9,9 @@ def map (source_array)
 end
 
 def reduce (source_array, starting_value = 0)
-  if (source_array == [ false, nil, nil, nil]) {
+  if (source_array == [ false, nil, nil, nil])
     puts "test"
-  }
+  end
   result = starting_value
   
   i = 0

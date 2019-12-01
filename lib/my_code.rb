3bfdd80cb1c65f1ca32bc918e465341101ce824a
Olena Matokhina <olena@matokhina.com>
2019-12-01 18:55:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,11 @@
 def map (source_array) 
-  new = []
-  
-  yield
-  
-  return new
+  result = []
+  i = 0 
+  while i < source_array.size do
+    result[i] = yield(source_array[i])
+    i += 1
+  end
+  return result
 end
 
 def reduce (source_array, starting_value = 0)

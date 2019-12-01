bad0a733b07b02b3e3ef68bee265f94648d41246
Olena Matokhina <olena@matokhina.com>
2019-12-01 18:57:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,6 +9,13 @@ def map (source_array)
 end
 
 def reduce (source_array, starting_value = 0)
+  result = starting_point
   
+  i = 0
+  while i < source_array.size do
+    result += source_array[i]
+    i += 1
+  end
+  return result
   
 end

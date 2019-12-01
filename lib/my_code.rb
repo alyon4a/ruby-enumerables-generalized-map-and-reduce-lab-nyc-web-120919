89b5648a2b98f363fd898a10d15a5dbb4d2e1c6b
Olena Matokhina <olena@matokhina.com>
2019-12-01 19:11:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,13 +9,16 @@ def map (source_array)
 end
 
 def reduce (source_array, starting_value = 0)
-  if (source_array == [ false, nil, nil, nil])
-    puts "test"
-  end
+  
   result = starting_value
   
   i = 0
   while i < source_array.size do
+    if (source_array == [ false, nil, nil, nil])
+      puts i
+      puts source_array[i]
+      puts result
+    end
     result = yield(source_array[i], result)
     i += 1
   end
